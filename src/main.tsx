4913f8efed542c1eeaca9cd588f2e2211e04206e
Max <max@max23.cn>
2026-07-23 08:48:12 +0800

    关闭桌面应用默认右键菜单

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@arco-design/web-react/dist/css/arco.css";
+import { isTauri } from "@tauri-apps/api/core";
 import App from "./App";
 import SettingsWindow from "./components/SettingsWindow";
+
+if (isTauri()) {
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
+}
 
 const view = new URLSearchParams(window.location.search).get("view");
 const RootView = view === "settings" ? SettingsWindow : App;
